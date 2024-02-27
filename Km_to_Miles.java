3cc4d49fbc0612b0c0c84481350e0722cc0ab941
ellas <ella.yap26@ycdsbk12.ca>
2024-02-27 12:53:06 -0500

    Updated Km_to_Miles.java

--- Km_to_Miles.java
+++ Km_to_Miles.java
@@ -2,13 +2,23 @@
 class Km_to_Miles extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Program Km_to_Miles.java lets you enter a distance in kilometers and that converts it into miles (1 km = 0.621371 miles).
+  * @author: E. Yap
   */
   
   public void run() {
     
-    // start coding here
+    // Variables
+    double dblKmDistance;
+    double dblMilesDistance;
+
+    // Get Km Distance
+    dblKmDistance = readDouble("Enter a distance in kilometers: ");
+
+    // Compute Miles Distance
+    dblMilesDistance = dblKmDistance * 0.621371;
     
+    // Output Km to Miles
+    System.out.println(dblKmDistance + " kilometers = " + dblMilesDistance + " miles");
   }
 }
