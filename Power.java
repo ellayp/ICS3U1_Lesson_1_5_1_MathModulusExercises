ad547cab0332bd7408f570198e2d81535e675efc
ellas <ella.yap26@ycdsbk12.ca>
2024-02-27 12:59:57 -0500

    Updated Power.java

--- Power.java
+++ Power.java
@@ -1,13 +1,25 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Program Power.java lets you enter two numbers a and b, and that prints out a to the power of b.
+  * @author: E. Yap
   */
   
   public void run() {
     
-    // start coding here
+    // Variables
+    double dblVariableA;
+    double dblVariableB;
+    double dblPower;
+
+    // Get Variables (A and B)
+    dblVariableA = readInt("Enter a number: ");
+    dblVariableB = readInt("Enter a second number: ");
+
+    // Compute Power
+    dblPower = Math.pow(dblVariableA, dblVariableB);
     
+    // Output
+    System.out.println(dblVariableA + " to the power of " + dblVariableB + " = " + dblPower);
   }
 }
