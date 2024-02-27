766b574b7f018aef14f8a492e7e4b67252fb72df
ellas <ella.yap26@ycdsbk12.ca>
2024-02-27 13:06:32 -0500

    Updated Hours.java

--- Hours.java
+++ Hours.java
@@ -1,13 +1,25 @@
 class Hours extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Program Hours.java lets you enter a number of hours, and that converts it to days and hours. For example, 111 hours = 4 days and 15 hours.
+  * @author: E. Yap
   */
   
   public void run() {
     
-    // start coding here
+    // Variables
+    int intHours;
+    int intDays;
+    int intLeftoverHours;
+
+    // Get Variable Hours
+    intHours = readInt("Enter a number of hours: ");
     
+    // Compute Days and Hours
+    intDays = intHours / 24;
+    intLeftoverHours = intHours % 24;
+
+    // Output Days and Hours
+    System.out.println(intHours + " hours = " + intDays + " days and " + intLeftoverHours + " hours ");
   }
 }
